c3d43fcfe92fec2d8836118c3df32af5a1364a11
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-09-29 20:41:40 -0500

    🐊 Inicio

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+if (is_singular()) {
+  header(
+    sprintf(
+      'Location: /wp-json/wp/v2/%s/%s',
+      get_post_type_object(get_post_type())->rest_base,
+      get_post()->ID
+    )
+  );
+} else {
+  header('Location: /wp-json/');
+}
